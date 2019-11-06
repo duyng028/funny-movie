fb86d117d423117a6132c1bdf1cb4b6b0a4ab673
Duy Nguyen <duy.nguyen@aitokyolab.com>
2019-11-06 14:17:12 +0700

    Check share page, when not logged in will be redirect to 404 page

--- src/App.js
+++ src/App.js
@@ -14,13 +14,14 @@ const routeConfig = [
     exact: true,
     component: HomePage
   },
-  { path: '/share', exact: true, component: ShareMoviePage },
+  { path: '/share', exact: true, component: ShareMoviePage, requiredAuth: true },
   { path: '/404', exact: true, component: NotFoundPage }
 ];
 
 const _generateRoutes = () => {
-  const routes = routeConfig.map(({ path, exact, component }, index) => {
-    const ComponentWithLayout = createPage(component);
+  const routes = routeConfig.map((route, index) => {
+    const { path, exact, component } = route;
+    const ComponentWithLayout = createPage(component, route);
     return <Route path={path} exact={exact} component={ComponentWithLayout} key={`route-${index + 1}`} />;
   });
 
--- src/containers/createPage.js
+++ src/containers/createPage.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Redirect } from 'react-router';
 import { withRouter } from 'react-router-dom';
 import { Header } from '../components';
 import '../styles/index.scss';
 
-export default function(ComposedComponent) {
+export default function(ComposedComponent, routeConfig = {}) {
   class Layout extends Component {
     render() {
       const { isLoggedIn } = this.props;
+      const { requiredAuth } = routeConfig;
+      if (requiredAuth && !isLoggedIn) return <Redirect to="/404" />;
+
       return (
         <div className="container">
           <Header isLoggedIn={isLoggedIn} />
